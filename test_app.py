403b757ff2a0db1ed451286752e7d359a1bd755e
Amit152116Kumar <amit170103004@iitg.ac.in>
2023-07-11 04:23:25 +0530

    Add Test & Update requirements.txt

---
+++ test_app.py
@@ -0,0 +1,25 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+from main import app
+
+client = TestClient(app)
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK
+    
+def test_livefeed():
+    response = client.get("/livefeed")
+    assert response.status_code == status.HTTP_200_OK
+    
+def test_orders():
+    response = client.get("/orders")
+    assert response.status_code == status.HTTP_200_OK
+    
+def test_trades():
+    response = client.get("/trades")
+    assert response.status_code == status.HTTP_200_OK
+
+def test_funds():
+    response = client.get("/funds")
+    assert response.status_code == status.HTTP_200_OK
